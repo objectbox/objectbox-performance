27af18c14fc59f9c0907acc15673aa84b820b4f9
Markus <markus@greenrobot>
2016-10-01 12:28:21 +0200

    basic UI

--- app/src/main/java/io/objectbox/performanceapp/MainActivity.java
+++ app/src/main/java/io/objectbox/performanceapp/MainActivity.java
@@ -2,12 +2,48 @@ package io.objectbox.performanceapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
+    TestType[] TYPES = {
+            new TestType(TestType.BULK_OPERATIONS),
+            new TestType(TestType.BULK_OPERATIONS_INDEXED),
+            new TestType(TestType.LOOK_UP_STRING_INDEX),
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.buttonRunTest).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, TYPES);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ((Spinner) findViewById(R.id.spinnerTestType)).setAdapter(adapter);
     }
+
+    class TestType {
+        public static final String BULK_OPERATIONS = "Bulk operations (CRUD)";
+        public static final String BULK_OPERATIONS_INDEXED = "Bulk operations (CRUD) - indexed";
+        public static final String LOOK_UP_STRING_INDEX = "Look up string using index";
+
+        String name;
+
+        public TestType(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
 }
