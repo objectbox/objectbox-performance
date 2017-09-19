2e15bb75bc0335248dfc1604a6701defc60c88e6
greenrobot Team <opensource+team@greenrobot>
2017-09-19 13:45:22 +0200

    Add Room basic CRUD batch test.

--- app/src/main/java/io/objectbox/performanceapp/room/RoomPerfTest.java
+++ app/src/main/java/io/objectbox/performanceapp/room/RoomPerfTest.java
@@ -4,6 +4,9 @@ import android.arch.persistence.room.Room;
 import android.content.Context;
 import android.database.Cursor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.objectbox.performanceapp.PerfTest;
 import io.objectbox.performanceapp.PerfTestRunner;
 import io.objectbox.performanceapp.TestType;
@@ -12,8 +15,10 @@ public class RoomPerfTest extends PerfTest {
 
     public static final String DB_NAME = "sqlite-room";
 
-    private AppDatabase db;
     private boolean versionLoggedOnce;
+    private AppDatabase db;
+    private SimpleEntityDao dao;
+    private SimpleEntityIndexedDao daoIndexed;
 
     @Override
     public String name() {
@@ -29,6 +34,8 @@ public class RoomPerfTest extends PerfTest {
         }
         db = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, DB_NAME)
                 .build();
+        dao = db.simpleEntityDao();
+        daoIndexed = db.simpleEntityIndexedDao();
 
         if (!versionLoggedOnce) {
             Cursor cursor = db.query("select sqlite_version() AS sqlite_version", null);
@@ -48,12 +55,12 @@ public class RoomPerfTest extends PerfTest {
     @Override
     public void run(TestType type) {
         switch (type.name) {
-//            case TestType.CRUD:
-//                runBatchPerfTest(false);
-//                break;
-//            case TestType.CRUD_SCALARS:
-//                runBatchPerfTest(true);
-//                break;
+            case TestType.CRUD:
+                runBatchPerfTest(false);
+                break;
+            case TestType.CRUD_SCALARS:
+                runBatchPerfTest(true);
+                break;
 //            case TestType.CRUD_INDEXED:
 //                runBatchPerfTestIndexed();
 //                break;
@@ -78,11 +85,92 @@ public class RoomPerfTest extends PerfTest {
         }
     }
 
+    private void runBatchPerfTest(boolean scalarsOnly) {
+        List<SimpleEntity> list = new ArrayList<>(numberEntities);
+        for (int i = 0; i < numberEntities; i++) {
+            list.add(createEntity((long) i, scalarsOnly));
+        }
+        startBenchmark("insert");
+        dao.insertInTx(list);
+        stopBenchmark();
+
+        for (SimpleEntity entity : list) {
+            if (scalarsOnly) {
+                setRandomScalars(entity);
+            } else {
+                setRandomValues(entity);
+            }
+        }
+        startBenchmark("update");
+        dao.updateInTx(list);
+        stopBenchmark();
+
+        //noinspection UnusedAssignment
+        list = null;
+
+        startBenchmark("load");
+        List<SimpleEntity> reloaded = dao.loadAll();
+        stopBenchmark();
+
+        startBenchmark("access");
+        accessAll(reloaded);
+        stopBenchmark();
+
+        startBenchmark("delete");
+        dao.deleteInTx(reloaded);
+        stopBenchmark();
+    }
+
     @Override
     public void tearDown() {
         super.tearDown();
         db.close();
         boolean deleted = context.deleteDatabase(DB_NAME);
         log("DB deleted: " + deleted);
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    protected void accessAll(List<SimpleEntity> list) {
+        for (SimpleEntity entity : list) {
+            entity.getId();
+            entity.getSimpleBoolean();
+            entity.getSimpleByte();
+            entity.getSimpleShort();
+            entity.getSimpleInt();
+            entity.getSimpleLong();
+            entity.getSimpleFloat();
+            entity.getSimpleDouble();
+            entity.getSimpleString();
+            entity.getSimpleByteArray();
+        }
+    }
+
+    private void setRandomValues(SimpleEntity entity) {
+        setRandomScalars(entity);
+        entity.setSimpleString(randomString());
+        entity.setSimpleByteArray(randomBytes());
+    }
+
+    private void setRandomScalars(SimpleEntity entity) {
+        entity.setSimpleBoolean(random.nextBoolean());
+        entity.setSimpleByte((byte) random.nextInt());
+        entity.setSimpleShort((short) random.nextInt());
+        entity.setSimpleInt(random.nextInt());
+        entity.setSimpleLong(random.nextLong());
+        entity.setSimpleDouble(random.nextDouble());
+        entity.setSimpleFloat(random.nextFloat());
+    }
+
+    private SimpleEntity createEntity(Long key, boolean scalarsOnly) {
+        SimpleEntity entity = new SimpleEntity();
+        if (key != null) {
+            entity.setId(key);
+        }
+        if (scalarsOnly) {
+            setRandomScalars(entity);
+        } else {
+            setRandomValues(entity);
+        }
+        return entity;
     }
 }
--- app/src/main/java/io/objectbox/performanceapp/room/SimpleEntityDao.java
+++ app/src/main/java/io/objectbox/performanceapp/room/SimpleEntityDao.java
@@ -3,14 +3,24 @@ package io.objectbox.performanceapp.room;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
 
 @Dao
 public interface SimpleEntityDao {
 
     @Insert
-    void insertAll(SimpleEntity... entities);
+    void insertInTx(List<SimpleEntity> entities);
+
+    @Query("SELECT * FROM simpleentity")
+    List<SimpleEntity> loadAll();
+
+    @Update
+    void updateInTx(List<SimpleEntity> entities);
 
     @Delete
-    void delete(SimpleEntity entity);
+    void deleteInTx(List<SimpleEntity> entities);
 
 }
