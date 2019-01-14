252cdf129988e73cb4752d4d3105e445e9463640
greenrobot Team <opensource+team@greenrobot>
2019-01-14 09:10:48 +0100

    Update Room [1.1.1 -> 2.0.0], also migrate to AndroidX.

--- app/src/main/java/io/objectbox/performanceapp/room/AppDatabase.java
+++ app/src/main/java/io/objectbox/performanceapp/room/AppDatabase.java
@@ -1,7 +1,7 @@
 package io.objectbox.performanceapp.room;
 
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.RoomDatabase;
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
 @Database(entities = {SimpleEntity.class, SimpleEntityIndexed.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
--- app/src/main/java/io/objectbox/performanceapp/room/SimpleEntity.java
+++ app/src/main/java/io/objectbox/performanceapp/room/SimpleEntity.java
@@ -1,7 +1,7 @@
 package io.objectbox.performanceapp.room;
 
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity
 public class SimpleEntity {
