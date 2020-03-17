8a93d02ee19b99fc009b059337fcabf79627b5a1
greenrobot Team <opensource+team@greenrobot>
2020-03-17 14:14:20 +0100

    Use view binding, remember last settings.

--- app/src/main/java/io/objectbox/performanceapp/MainActivity.java
+++ app/src/main/java/io/objectbox/performanceapp/MainActivity.java
@@ -18,38 +18,41 @@ package io.objectbox.performanceapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import io.objectbox.performanceapp.PerfTestRunner.Callback;
+import io.objectbox.performanceapp.databinding.ActivityMainBinding;
 import io.objectbox.performanceapp.greendao.GreendaoPerfTest;
 import io.objectbox.performanceapp.objectbox.ObjectBoxPerfTest;
 import io.objectbox.performanceapp.realm.RealmPerfTest;
 import io.objectbox.performanceapp.room.RoomPerfTest;
 
 public class MainActivity extends Activity implements Callback {
-    private TextView textViewResults;
-    private Button buttonRun;
+
+    private static final String PREF_TYPE = "io.objectbox.performance.type";
+    private static final String PREF_RUNS = "io.objectbox.performance.runs";
+    private static final String PREF_COUNT = "io.objectbox.performance.count";
+
+    private ActivityMainBinding binding;
     private PerfTestRunner testRunner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        buttonRun = (Button) findViewById(R.id.buttonRunTest);
-        buttonRun.setOnClickListener(view -> {
-            buttonRun.setEnabled(false);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonRunTest.setOnClickListener(view -> {
+            binding.buttonRunTest.setEnabled(false);
             View currentFocus = MainActivity.this.getCurrentFocus();
             if (currentFocus != null) {
                 InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -58,28 +61,56 @@ public class MainActivity extends Activity implements Callback {
                 }
                 currentFocus.clearFocus();
             }
-            boolean objectBox = ((CheckBox) findViewById(R.id.checkBoxObjectBox)).isChecked();
-            boolean realm = ((CheckBox) findViewById(R.id.checkBoxRealm)).isChecked();
-            boolean greenDao = ((CheckBox) findViewById(R.id.checkBoxGreenDao)).isChecked();
-            boolean room = ((CheckBox) findViewById(R.id.checkBoxRoom)).isChecked();
-            TestType type = (TestType) ((Spinner) findViewById(R.id.spinnerTestType)).getSelectedItem();
+            boolean objectBox = binding.checkBoxObjectBox.isChecked();
+            boolean realm = binding.checkBoxRealm.isChecked();
+            boolean greenDao = binding.checkBoxGreenDao.isChecked();
+            boolean room = binding.checkBoxRoom.isChecked();
+            TestType type = (TestType) binding.spinnerTestType.getSelectedItem();
 
-            int runs;
-            int numberEntities;
-            try {
-                runs = Integer.parseInt(((EditText) findViewById(R.id.editTextRuns)).getText().toString());
-                numberEntities =
-                        Integer.parseInt(((EditText) findViewById(R.id.editTextNumberEntities)).getText().toString());
-            } catch (NumberFormatException e) {
-                textViewResults.append(e.getMessage() + "\n");
-                return;
-            }
+            int runs = getIntegerFromEditTextOrZero(binding.editTextRuns);
+            int numberEntities = getIntegerFromEditTextOrZero(binding.editTextNumberEntities);
+
             runTests(type, runs, numberEntities, objectBox, realm, greenDao, room);
         });
-        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, TestType.ALL);
+
+        ArrayAdapter adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                TestType.ALL
+        );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        ((Spinner) findViewById(R.id.spinnerTestType)).setAdapter(adapter);
-        textViewResults = ((TextView) findViewById(R.id.textViewResults));
+        binding.spinnerTestType.setAdapter(adapter);
+
+        // Restore type, runs and count or set defaults.
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        binding.spinnerTestType
+                .setSelection(prefs.getInt(PREF_TYPE, 1), false);
+        binding.editTextRuns
+                .setText(String.valueOf(prefs.getInt(PREF_RUNS, 1)));
+        binding.editTextNumberEntities
+                .setText(String.valueOf(prefs.getInt(PREF_COUNT, 100000)));
+    }
+
+    private int getIntegerFromEditTextOrZero(EditText editText) {
+        try {
+            return Integer.parseInt(editText.getText().toString());
+        } catch (NumberFormatException e) {
+            binding.textViewResults.append(e.getMessage() + "\n");
+            return 0;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Save type, runs and count.
+        PreferenceManager.getDefaultSharedPreferences(this).edit()
+                .putInt(PREF_TYPE, binding.spinnerTestType.getSelectedItemPosition())
+                .putInt(PREF_RUNS,
+                        getIntegerFromEditTextOrZero(binding.editTextRuns))
+                .putInt(PREF_COUNT,
+                        getIntegerFromEditTextOrZero(binding.editTextNumberEntities))
+                .apply();
     }
 
     @Override
@@ -92,7 +123,7 @@ public class MainActivity extends Activity implements Callback {
     }
 
     private void runTests(TestType type, int runs, int numberEntities, boolean objectBox, boolean realm, boolean greenDao, boolean room) {
-        textViewResults.setText("");
+        binding.textViewResults.setText("");
         List<PerfTest> tests = new ArrayList<>();
         if (objectBox) {
             tests.add(new ObjectBoxPerfTest());
@@ -106,14 +137,14 @@ public class MainActivity extends Activity implements Callback {
         if (room) {
             tests.add(new RoomPerfTest());
         }
-        testRunner = new PerfTestRunner(this, this, textViewResults, runs, numberEntities);
+        testRunner = new PerfTestRunner(this, this, binding.textViewResults, runs, numberEntities);
         testRunner.run(type, tests);
     }
 
     @Override
     public void done() {
         testRunner = null;
-        runOnUiThread(() -> buttonRun.setEnabled(true));
+        runOnUiThread(() -> binding.buttonRunTest.setEnabled(true));
     }
 
 }
