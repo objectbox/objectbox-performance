169bbfe3186afe929545123a589957a596232150
greenrobot Team <opensource+team@greenrobot>
2024-05-22 10:29:44 +0200

    KTS: migrate build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,32 +1,22 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    // https://github.com/ben-manes/gradle-versions-plugin/releases
+    id("com.github.ben-manes.versions") version "0.39.0"
+}
 
 buildscript {
-    ext.objectboxVersion = "4.0.0"
+    val objectboxVersion by extra("4.0.0")
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath('com.android.tools.build:gradle:8.4.1')
+        classpath("com.android.tools.build:gradle:8.4.1")
         classpath("io.objectbox:objectbox-gradle-plugin:$objectboxVersion")
         classpath("io.realm:realm-gradle-plugin:10.18.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
-    }
-}
-
-plugins {
-    id "com.github.ben-manes.versions" version "0.39.0"
-}
-def isNonStable = { String version ->
-    def stableKeyword = ['RELEASE', 'FINAL', 'GA'].any { it -> version.toUpperCase().contains(it) }
-    def regex = /^[0-9,.v-]+(-r)?$/
-    return !stableKeyword && !(version ==~ regex)
-}
-tasks.named("dependencyUpdates").configure {
-    rejectVersionIf {
-        isNonStable(it.candidate.version)
     }
 }
 
@@ -37,12 +27,19 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+// Reject preview releases for dependencyUpdates task
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
 }
 
-wrapper {
-    group = "build setup"
-    //noinspection UnnecessaryQualifiedReference
+tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
 }
